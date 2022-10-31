23b65c5cbcaeb2ece6026187ef730daa4a6f38c3
Matheus Ricardo <matheu_ric99@hotmail.com>
2022-10-31 11:31:12 -0300

    feat:criando template

---
+++ src/errors/appError.ts
@@ -0,0 +1,10 @@
+class AppError extends Error {
+  statusCode;
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
