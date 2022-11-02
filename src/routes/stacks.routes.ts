c4754edaec29ce05eda55a551dab7c5dd8560dd3
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-02 20:56:49 -0300

    refactor: implementation validateSerializers

--- src/routes/stacks.routes.ts
+++ src/routes/stacks.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express"
 
+import validadeSerializerMiddleware from "../middlewares/validateSerializer.middleware"
+
+import { createBudgetStackSerializer } from "../serializers/budgetStack.serializer"
+
 const routes = Router()
 
 export const stacksRouter = () => {
-  routes.post("/")
+  routes.post(
+    "/",
+    validadeSerializerMiddleware(createBudgetStackSerializer),
+  )
   routes.get("/")
   routes.get("/:id")
   routes.delete("/:id")
