a37f44313866a93fbd5b415e88055f6409d82c95
Alvaro Alencar <alvaroromario1@hotmail.com>
2022-11-03 02:30:35 -0300

    feat: add delete customer functionality

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 
 import createCustomerController from "../controllers/customers/createCustomer.controller"
+import deleteCustomerController from "../controllers/customers/deleteCustomer.controller"
 import listCustomersController from "../controllers/customers/listCustomers.controller"
 import listOneCustomerController from "../controllers/customers/listOneCustomer.controller"
+import updateCustomerController from "../controllers/customers/updateCustomer.controller"
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware"
 
 const routes = Router()
@@ -11,8 +13,8 @@ export const costumersRouter = () => {
   routes.post("/", createCustomerController)
   routes.get("/", ensureAuthMiddleware, listCustomersController)
   routes.get("/:id", ensureAuthMiddleware, listOneCustomerController)
-  routes.patch("/:id", ensureAuthMiddleware)
-  routes.delete("/:id", ensureAuthMiddleware)
+  routes.patch("/:id", ensureAuthMiddleware, updateCustomerController)
+  routes.delete("/:id", ensureAuthMiddleware, deleteCustomerController)
 
   return routes
 }
