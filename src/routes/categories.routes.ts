da43f142f9e0176a007f039b6ea796ed3f3d0f42
terragabriella <gabriellaterra8@gmail.com>
2022-11-03 14:49:07 -0300

    fix:checar se as rotas e seus middlewares estão corretos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,19 @@
 import { Router } from "express"
-
 import createCategoriesControllers from "../controllers/categories/createCategory.controllers"
 import deleteCategoryControllers from "../controllers/categories/deleteCategory.controllers"
 import listCategoriesControllers from "../controllers/categories/listCategories.controllers"
 import listOneCategoryControllers from "../controllers/categories/listOneCatgory.controllers"
-
+import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware"
 import validadeSerializerMiddleware from "../middlewares/validateSerializer.middleware"
-
 import { createCategorySerializer } from "../serializers/category.serializer"
 
 const routes = Router()
 
 export const categoriesRouter = () => {
-  routes.post(
-    "/", 
-    validadeSerializerMiddleware(createCategorySerializer),
-    createCategoriesControllers
-  )
-  routes.get("/", listCategoriesControllers)
-  routes.get("/:id", listOneCategoryControllers)
-  routes.delete("/:id", deleteCategoryControllers)
+  routes.post("/", validadeSerializerMiddleware(createCategorySerializer),ensureAuthMiddleware,createCategoriesControllers)
+  routes.get("/",ensureAuthMiddleware, listCategoriesControllers)
+  routes.get("/:id",ensureAuthMiddleware, listOneCategoryControllers)
+  routes.delete("/:id",ensureAuthMiddleware, deleteCategoryControllers)
 
   return routes
 }
--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from "express"
 import validadeSerializerMiddleware from "../middlewares/validateSerializer.middleware"
-
 import createSessionController from "../controllers/login/sessions.controller"
-
 import { sessionSerializer } from "../serializers/session.serializer"
 
 const routes = Router()
 
 export const sessionsRouter = () => {
-  routes.post(
-    "/", 
-    validadeSerializerMiddleware(sessionSerializer), 
-    createSessionController
-  )
+  routes.post("/", validadeSerializerMiddleware(sessionSerializer), createSessionController)
   
   return routes
 }
