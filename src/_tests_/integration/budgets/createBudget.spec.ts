a57f20c719364f9435990c15e048e703263b47bb
Bruna <bruna.dall9741@gmail.com>
2022-11-03 13:05:05 -0300

    feat: create tests base structure

---
+++ src/_tests_/integration/budgets/createBudget.spec.ts
@@ -0,0 +1,111 @@
+import { DataSource } from "typeorm"
+import request from "supertest"
+
+import AppDataSource from "../../../data-source"
+import app from "../../../app"
+
+import { mockedBudget, mockedUser, mockedUserLogin } from "../../mocks"
+
+import * as uuid from "uuid"
+jest.mock("uuid")
+
+let tokenUser = ""
+
+describe("POST - /budgets/", () => {
+  let connection: DataSource
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+      })
+  })
+
+  afterAll(async () => {
+    await connection.destroy()
+  })
+
+  test("Shouldn't be possible to create a new budget without authentication", async () => {
+    const errAuthUser = await request(app).post("/budgets").send(mockedBudget)
+
+    expect(errAuthUser.status).toBe(401)
+    expect(errAuthUser.body).toMatchObject({
+      message: "missing token",
+    })
+  })
+
+  test("Shouldn't be possible to create a new budget with a customer that does not exist", async () => {
+    await request(app).post("/users").send(mockedUser)
+
+    const resLogin = await request(app).post("/login").send(mockedUserLogin)
+    tokenUser = resLogin.body.token
+
+    const budgetData = { ...mockedBudget, customerId: "invalid_id" }
+
+    const resCreateBudget = await request(app)
+      .post("/budgets")
+      .set("Authorization", `Bearer ${tokenUser}`)
+      .send(budgetData)
+
+    expect(resCreateBudget.status).toBe(404)
+    expect(resCreateBudget.body).toMatchObject({
+      message: "Customer not found.",
+    })
+  })
+
+  test("Shouldn't be possible to create a new budget with a budget stack that does not exist", async () => {
+    const budgetData = { ...mockedBudget, budgetStackId: "invalid_id" }
+
+    const resCreateBudget = await request(app)
+      .post("/budgets")
+      .set("Authorization", `Bearer ${tokenUser}`)
+      .send(budgetData)
+
+    expect(resCreateBudget.status).toBe(404)
+    expect(resCreateBudget.body).toMatchObject({
+      message: "Stack not found.",
+    })
+  })
+
+  test("Shouldn't be possible to create a new budget with a category that does not exist", async () => {
+    const budgetData = { ...mockedBudget, categoryId: "invalid_id" }
+
+    const resCreateBudget = await request(app)
+      .post("/budgets")
+      .set("Authorization", `Bearer ${tokenUser}`)
+      .send(budgetData)
+
+    expect(resCreateBudget.status).toBe(404)
+    expect(resCreateBudget.body).toMatchObject({
+      message: "Category not found.",
+    })
+  })
+
+  test("Should be possible to create a new budget", async () => {
+    const uuidSpy = jest.spyOn(uuid, "v4")
+    uuidSpy.mockReturnValue("some-uuid")
+
+    const resCreateBudget = await request(app)
+      .post("/budgets")
+      .set("Authorization", `Bearer ${tokenUser}`)
+      .send(mockedBudget)
+
+    expect(resCreateBudget.status).toBe(201)
+    expect(uuidSpy).toHaveBeenCalled()
+    expect(resCreateBudget.body).toEqual(
+      expect.objectContaining({
+        uuid: "some-uuid",
+        projectName: "Kenzie News",
+        projectTime: 25,
+        budget: 4500,
+        fixedCost: 4000,
+        variableCos: 500,
+        userId: "",
+        categoryId: "",
+        customerId: "",
+        budgetStackId: "",
+      })
+    )
+  })
+})
---
+++ src/_tests_/integration/budgets/deleteBudget.spec.ts
@@ -0,0 +1,89 @@
+import { DataSource } from "typeorm"
+import request from "supertest"
+
+import AppDataSource from "../../../data-source"
+import app from "../../../app"
+
+import { mockedBudget, mockedUser, mockedUserLogin } from "../../mocks"
+
+let tokenUser = ""
+let budgetId = ""
+
+describe("DELETE - /budgets/:uuid/ ", () => {
+  let connection: DataSource
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+      })
+
+    await request(app).post("/users").send(mockedUser)
+
+    const resLogin = await request(app).post("/login").send(mockedUserLogin)
+    tokenUser = resLogin.body.token
+
+    const resCreateBudget = await request(app)
+      .post("/budgets")
+      .set("Authorization", `Bearer ${tokenUser}`)
+      .send(mockedBudget)
+
+    budgetId = resCreateBudget.body.uuid
+  })
+
+  afterAll(async () => {
+    await connection.destroy()
+  })
+
+  test("Shouldn't be possible to delete a budget without authentication", async () => {
+    const errAuthUser = await request(app).delete(`/budgets/${budgetId}`)
+
+    expect(errAuthUser.status).toBe(401)
+    expect(errAuthUser.body).toMatchObject({
+      message: "missing token",
+    })
+  })
+
+  test("Shouldn't be possible to delete a budget that does not exist", async () => {
+    const resDeleteBudget = await request(app)
+      .delete("/budgets/invalid_id")
+      .set("Authorization", `Bearer ${tokenUser}`)
+
+    expect(resDeleteBudget.status).toBe(404)
+    expect(resDeleteBudget.body).toMatchObject({
+      message: "Budget not found.",
+    })
+  })
+
+  test("Shouldn't be possible to delete a budget from another user", async () => {
+    await request(app)
+      .post("/users")
+      .send({ ...mockedUser, email: "anotherUser@email.com" })
+
+    const resLogin = await request(app)
+      .post("/login")
+      .send({ ...mockedUserLogin, email: "anotherUser@email.com" })
+    const differentToken = resLogin.body.token
+
+    const resDeleteBudget = await request(app)
+      .delete(`/budgets/${budgetId}`)
+      .set("Authorization", `Bearer ${differentToken}`)
+
+    expect(resDeleteBudget.status).toBe(401)
+    expect(resDeleteBudget.body).toMatchObject({
+      message: "Unauthorized access.",
+    })
+  })
+
+  test("Should be possible to delete the budget", async () => {
+    const resDeleteBudget = await request(app)
+      .delete(`/budgets/${budgetId}`)
+      .set("Authorization", `Bearer ${tokenUser}`)
+
+    expect(resDeleteBudget.status).toBe(204)
+    expect(resDeleteBudget.body).toMatchObject({
+      message: "Budget deleted!",
+    })
+  })
+})
