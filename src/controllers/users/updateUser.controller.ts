ec7afcd51d874d79baa2d1317b510eeba6792437
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-03 11:57:59 -0300

    refactor: implementing instanceToPlain

--- src/controllers/users/updateUser.controller.ts
+++ src/controllers/users/updateUser.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express"
 import updateUserService from "../../services/users/updateUser.service"
+import { instanceToPlain } from 'class-transformer';
 
 const updateUserController = async (request: Request, response: Response) => {
     const data = request.body
     const id = request.params.id
     const userUpdated = await updateUserService(data, id)
-    const { password, ...userResponse} = userUpdated
-    return response.status(200).json(userResponse)
+
+    return response.status(200).json(instanceToPlain(userUpdated))
 }
 
 export default updateUserController
