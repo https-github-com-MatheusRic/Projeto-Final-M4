c55c283059d36164729f4eb621c081b32d27b043
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-02 21:05:27 -0300

    refactor: validation of some more keys

--- src/serializers/user.serializer.ts
+++ src/serializers/user.serializer.ts
@@ -1,10 +1,14 @@
 import * as yup from "yup"
 
 export const createUserSerializer = yup.object().shape({
-    email: yup.string().required(),
+    email: yup.string().email().required(),
     name: yup.string().required(),
     password: yup.string().required(),
     username: yup.string().required(),
     position: yup.string().required(),
-    imageUrl: yup.string().notRequired()
+    imageUrl: yup
+        .string()
+        .matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+    'Enter correct url!')
+        .notRequired()
 })
