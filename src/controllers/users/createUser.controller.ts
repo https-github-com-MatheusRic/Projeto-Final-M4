369329d903b036cbed6669a4a78ecca966b675cd
Álvaro Alencar <alvaroromario1@hotmail.com>
2022-11-03 02:37:51 -0300

    Revert "Feat/customers functionalities"

--- src/controllers/users/createUser.controller.ts
+++ src/controllers/users/createUser.controller.ts
@@ -6,7 +6,7 @@ import { instanceToPlain } from "class-transformer"
 const createUserControler = async (request: Request, response: Response) => {
     const userData = request.body
     const userCreated = await createUserService(userData)
-    return response.status(201).json(instanceToPlain(userCreated))
+    return response.status(201).json(userCreated)
 }
 
 export default createUserControler
