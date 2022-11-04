58a5e7af9bba9b7683ee5d7899908c25deed954f
Matheus Ricardo <matheu_ric99@hotmail.com>
2022-11-04 15:30:46 -0300

    fix:Solving Tests bugs

--- src/_tests_/mocks/index.ts
+++ src/_tests_/mocks/index.ts
@@ -1,8 +1,8 @@
-import { IUserCreate, IUserLogin } from "../../interfaces/users"
-import { IBudgetRequest, IBudgetUpdate } from "../../interfaces/budgets"
-import { ICustomerRequest, ICustomerUpdate } from "../../interfaces/customers"
-import { IBudgetStack } from "../../interfaces/budgetsStacks"
-import { ICategoryRequest } from "../../interfaces/categories"
+import { IUserCreate, IUserLogin, IUserUpdate } from "../../interfaces/users";
+import { IBudgetRequest, IBudgetUpdate } from "../../interfaces/budgets";
+import { ICustomerRequest, ICustomerUpdate } from "../../interfaces/customers";
+import { IBudgetStack } from "../../interfaces/budgetsStacks";
+import { ICategoryRequest } from "../../interfaces/categories";
 
 export const mockedUser: IUserCreate = {
   email: "test@email.com",
@@ -24,21 +24,21 @@ export const mockedUserUpdate: IUserUpdate = {
 export const mockedCustomer: ICustomerRequest = {
   name: "Gabriel",
   isCompany: false,
-  email: 'gabriel@email.com',
-  contact: '5511988888888'
-}
+  email: "gabriel@email.com",
+  contact: "5511988888888",
+};
 
 export const mockedCustomerUpdate: ICustomerUpdate = {
   name: "Gabriel A.",
-}
+};
 
 export const mockedBudgetStack: IBudgetStack = {
   stack: "Full Stack",
-}
+};
 
 export const mockedCategory: ICategoryRequest = {
   name: "Gabriel A.",
-}
+};
 
 export const mockedBudget: IBudgetRequest = {
   projectName: "Kenzie News",
