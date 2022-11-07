df8e08a534f41b31c98ff0d682b718c2ef45ad92
Bruna <bruna.dall9741@gmail.com>
2022-11-07 12:20:10 -0300

    feat: create categories tests

--- src/controllers/categories/listCategories.controllers.ts
+++ src/controllers/categories/listCategories.controllers.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express"
 import listCategoriesServices from "../../services/categories/listCategories.services"
 
-const listCategoriesControllers = async (req: Request ,  res: Response) => {
-    const listedCategories = await listCategoriesServices()
+const listCategoriesControllers = async (req: Request, res: Response) => {
+  const listedCategories = await listCategoriesServices()
 
-    res.status(200).json(listedCategories)
-
+  res.status(200).json(listedCategories)
 }
 export default listCategoriesControllers
