f27c48237035ed0993ca916dc7d6910ddfd65c1a
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-07 22:18:38 -0300

    perf: ESLint

--- src/services/login/sessions.service.ts
+++ src/services/login/sessions.service.ts
@@ -1,33 +1,33 @@
-import AppDataSource from "../../data-source"
-import { IUserLogin } from "../../interfaces/users"
-import AppError from "../../errors/appError"
-import { compare } from "bcryptjs"
-import jwt from 'jsonwebtoken'
-import { User } from "../../entities/user.entitie"
+import AppDataSource from "../../data-source";
+import { IUserLogin } from "../../interfaces/users";
+import AppError from "../../errors/appError";
+import { compare } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../../entities/user.entitie";
 
-const createSessionService = async ({email, password}: IUserLogin): Promise<string> => {
+const createSessionService = async ({
+  email,
+  password,
+}: IUserLogin): Promise<string> => {
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOneBy({
+    email: email,
+  });
 
-    const userRepository = AppDataSource.getRepository(User)
-    const user = await userRepository.findOneBy({
-        email: email
-    })
+  if (!user) {
+    throw new AppError("Invalid user or password", 403);
+  }
+  const passwordValidation = await compare(password, user.password);
+  if (!passwordValidation) {
+    throw new AppError("Invalid user or password", 403);
+  }
 
-    if(!user){
-        throw new AppError('Invalid user or password', 403)
-    }
-    const passwordValidation =  await compare(password, user.password)
-    if(!passwordValidation){
-        throw new AppError('Invalid user or password', 403)
-    }
+  const token = jwt.sign({}, String(process.env.SECRET_KEY), {
+    expiresIn: "24h",
+    subject: user.uuid,
+  });
 
-    const token = jwt.sign({},
-    String(process.env.SECRET_KEY),
-    {
-        expiresIn: '24h',
-        subject: user.uuid
-    })
-    
-    return token
-}
+  return token;
+};
 
-export default createSessionService
+export default createSessionService;
