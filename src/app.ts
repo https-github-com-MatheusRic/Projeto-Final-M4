fae28bc5d4560bff852e240a3cd3b837f03d8076
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-09 10:21:29 -0300

    fix: test cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import "express-async-errors"
 
 import { appRoutes } from "./routes"
 import handleErrorMiddleware from "./middlewares/handleError.middleware"
+let cors = require('cors');
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 appRoutes(app)
